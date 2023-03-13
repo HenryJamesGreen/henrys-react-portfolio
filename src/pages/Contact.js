7649d60a6b3e4a63f2c573ef07aaa7b999e258e6
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-03-13 22:50:38 +0000

    added form

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,6 @@ import Card from "@mui/material/Card";
 import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import ButtonGroup from "@mui/material/ButtonGroup";
@@ -13,6 +12,8 @@ import Paper from "@mui/material/Paper";
 import Grid from "@mui/material/Grid";
 import { pink } from "@mui/material/colors";
 import SvgIcon from "@mui/material/SvgIcon";
+import { TextField } from "@mui/material";
+import { Email } from "@mui/icons-material";
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
@@ -33,12 +34,12 @@ function Contact() {
         spacing={{ xs: 2, md: 3 }}
         columns={{ xs: 4, sm: 8, md: 12 }}
       >
-        <Grid item xs={4} sm={4} md={4}>
+        <Grid item xs={12} sm={6} md={4}>
           <a href="https://github.com/HenryJamesGreen" target={"_blank"}>
             <Item>GitHub</Item>
           </a>
         </Grid>
-        <Grid item xs={4} sm={4} md={4}>
+        <Grid item xs={12} sm={6} md={4}>
           <a
             href="https://www.linkedin.com/in/henryjamesgreen/"
             target={"_blank"}
@@ -47,6 +48,83 @@ function Contact() {
           </a>
         </Grid>
       </Grid>
+      <Card style={{ maxWidth: 450, margin: "0 auto", padding: "20px 5px" }}>
+        <CardContent>
+          <Typography gutterBottom variant="h5">
+            Contact Me
+          </Typography>
+          <Typography
+            gutterBotom
+            color="secondary"
+            variant="body2"
+            component="p"
+          >
+            Fill out the form and I'll get back to you as soon as possible!
+          </Typography>
+          <form>
+            <Grid container spacing={1}>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  label="First Name"
+                  placeholder="Enter first name"
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} sm={6} item>
+                <TextField
+                  label="Last Name"
+                  placeholder="Enter last name."
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField
+                  type={Email}
+                  label="Email"
+                  placeholder="Enter email."
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField
+                  type="number"
+                  label="Phone"
+                  placeholder="Enter phone number."
+                  variant="outlined"
+                  fullWidth
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <TextField
+                  label="message"
+                  multiline
+                  rows={4}
+                  laceholder="Type your message here."
+                  variant="outlined"
+                  fullWidth
+                  required
+                />
+              </Grid>
+              <Grid xs={12} item>
+                <Button
+                  type="submit"
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                >
+                  Submit
+                </Button>
+              </Grid>
+            </Grid>
+          </form>
+        </CardContent>
+      </Card>
     </Box>
   );
 }
