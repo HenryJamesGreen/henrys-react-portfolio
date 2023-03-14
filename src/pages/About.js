281363096a3bd653cfe1746e956786ee650aaad2
HenryJamesGreen <henryjamesgreenmusic@gmail.com>
2023-03-14 00:12:41 +0000

    trying to fix images but running into problems with symlinking

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,9 @@
 import React from "react";
 import Card from "@mui/material/Card";
-import CardActions from "@mui/material/CardActions";
 import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
-import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
-import myImage from "../images/oldheadshot.jpeg";
+import myImage from "../images/oldheadshot.jpg";
 
 function About() {
   return (
